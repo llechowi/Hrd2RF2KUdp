8977784f91c17b909a730a51ec3c1f1464a14a11
les23 <les23@LAPTOP-DNP0RDDE>
2025-11-07 20:01:50 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Hrd2Udp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRF2KAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRF2KPort = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HRD IP Address";
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(285, 47);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(176, 31);
+            this.txtIpAddress.TabIndex = 1;
+            this.txtIpAddress.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(481, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(551, 47);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 31);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.Text = "7809";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(700, 47);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(124, 62);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(839, 47);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(137, 62);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.AcceptsTab = true;
+            this.txtStatus.Location = new System.Drawing.Point(41, 153);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.Size = new System.Drawing.Size(947, 182);
+            this.txtStatus.TabIndex = 12;
+            this.txtStatus.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "RF2K-S IP Address";
+            // 
+            // txtRF2KAddress
+            // 
+            this.txtRF2KAddress.Location = new System.Drawing.Point(285, 100);
+            this.txtRF2KAddress.Name = "txtRF2KAddress";
+            this.txtRF2KAddress.Size = new System.Drawing.Size(176, 31);
+            this.txtRF2KAddress.TabIndex = 14;
+            this.txtRF2KAddress.Text = "192.168.68.67";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(481, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Port";
+            // 
+            // txtRF2KPort
+            // 
+            this.txtRF2KPort.Location = new System.Drawing.Point(551, 103);
+            this.txtRF2KPort.Name = "txtRF2KPort";
+            this.txtRF2KPort.Size = new System.Drawing.Size(100, 31);
+            this.txtRF2KPort.TabIndex = 16;
+            this.txtRF2KPort.Text = "12060";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 60000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 375);
+            this.Controls.Add(this.txtRF2KPort);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtRF2KAddress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIpAddress);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "HRD to RF2K-S UDP Bridge";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox txtStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRF2KAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRF2KPort;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Hrd2Udp
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient tcpClient;
+        private UdpClient udpClient;
+        private NetworkStream stream;
+        private string udpAddr;
+        private int udpPort;
+
+        private uint prevFreq = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeUI();
+            LoadSettings();
+        }
+
+        private void InitializeUI()
+        {
+            txtIpAddress.Text = "127.0.0.1";
+            txtPort.Text = "7809";
+            txtRF2KAddress.Text = "192.168.68.67";
+            txtRF2KPort.Text = "12060";
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void LoadSettings()
+        {
+            txtIpAddress.Text = Properties.Settings.Default.HRDAddress;
+            txtPort.Text = Properties.Settings.Default.HRDPort.ToString();
+            txtRF2KAddress.Text = Properties.Settings.Default.RF2KAddress;
+            txtRF2KPort.Text = Properties.Settings.Default.RF2KPort.ToString();
+        }
+
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.HRDAddress = txtIpAddress.Text.Trim();
+            Properties.Settings.Default.HRDPort = int.Parse(txtPort.Text.Trim());
+            Properties.Settings.Default.RF2KAddress = txtRF2KAddress.Text.Trim();
+            Properties.Settings.Default.RF2KPort = int.Parse(txtRF2KPort.Text.Trim());
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ipAddress = txtIpAddress.Text;
+                int port = int.Parse(txtPort.Text);
+
+                tcpClient = new TcpClient();
+                tcpClient.Connect(ipAddress, port);
+                stream = tcpClient.GetStream();
+
+                udpClient = new UdpClient();
+                udpAddr = txtRF2KAddress.Text.Trim();
+                udpPort = int.Parse(txtRF2KPort.Text.Trim());
+
+                txtStatus.AppendText($"[{DateTime.Now:HH:mm:ss}] Connected to {ipAddress}:{port}\n\n");
+
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                txtIpAddress.Enabled = false;
+                txtPort.Enabled = false;
+                txtRF2KAddress.Enabled = false;
+                txtRF2KPort.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection failed: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //private void btnSend_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        if (stream != null && client.Connected)
+        //        {
+        //            // Create and send the message
+        //            string msg = txtMessage.Text.Trim().Replace(';', '\x09');
+        //            byte[] messageBytes = CreateMessage(msg);
+        //            stream.Write(messageBytes, 0, messageBytes.Length);
+        //            stream.Flush();
+
+        //            txtStatus.AppendText($"[{DateTime.Now:HH:mm:ss}] Sent: {txtMessage.Text}\n");
+
+        //            // Receive the response
+        //            byte[] response = ReceiveMessage();
+        //            if (response != null)
+        //            {
+        //                string responseText = ParseMessage(response);
+        //                //txtResponse.AppendText($"[{DateTime.Now:HH:mm:ss}] Received: {responseText}\n\n");
+        //                txtStatus.AppendText($"{responseText}\n\n");
+        //            }
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("Not connected to server", "Warning",
+        //                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Communication error: {ex.Message}", "Error",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
+        private byte[] CreateMessage(string text)
+        {
+            // Encode the text in UTF-16 with null terminator
+            string textWithNull = text + "\0";
+            byte[] szText = Encoding.Unicode.GetBytes(textWithNull);
+
+            // Calculate total message size
+            int nSize = szText.Length+16;
+            uint nSanity1 = 0x1234ABCD;
+            uint nSanity2 = 0xABCD1234;
+            uint nChecksum = 0;
+
+            // Create the message buffer
+            using (MemoryStream ms = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(ms))
+            {
+                writer.Write(nSize);        // 4 bytes
+                writer.Write(nSanity1);     // 4 bytes
+                writer.Write(nSanity2);     // 4 bytes
+                writer.Write(nChecksum);    // 4 bytes
+                writer.Write(szText);       // variable length
+
+                return ms.ToArray();
+            }
+        }
+
+        private byte[] ReceiveMessage()
+        {
+            try
+            {
+                //byte[] message = new byte[200];
+                //int bytesRead = 0;
+                //int read = stream.Read(message, bytesRead, 16);
+                // Read the header (16 bytes)
+                byte[] header = new byte[16];
+                int bytesRead = 0;
+                while (bytesRead < 16)
+                {
+                    int read = stream.Read(header, bytesRead, 16 - bytesRead);
+                    if (read == 0)
+                        throw new Exception("Connection closed by server");
+                    bytesRead += read;
+                }
+
+                // Parse header
+                int nSize = BitConverter.ToInt32(header, 0);
+                uint nSanity1 = BitConverter.ToUInt32(header, 4);
+                uint nSanity2 = BitConverter.ToUInt32(header, 8);
+                uint nChecksum = BitConverter.ToUInt32(header, 12);
+
+                // Validate magic numbers
+                if (nSanity1 != 0x1234ABCD || nSanity2 != 0xABCD1234)
+                {
+                    throw new Exception("Invalid message format - magic numbers don't match");
+                }
+
+                // Read the message text
+                int txtSize = nSize - 16;
+                byte[] szText = new byte[txtSize];
+                bytesRead = 0;
+                while (bytesRead < txtSize)
+                {
+                    int read = stream.Read(szText, bytesRead, txtSize - bytesRead);
+                    if (read == 0)
+                        throw new Exception("Connection closed while reading message");
+                    bytesRead += read;
+                }
+
+                return szText;
+            }
+            catch (Exception ex)
+            {
+                txtStatus.AppendText($"[{DateTime.Now:HH:mm:ss}] Receive error: {ex.Message}\n\n");
+                return null;
+            }
+        }
+
+        // returns the current TX frequency
+        private uint GetTXFrequency(byte[] szText)
+        {
+            // Decode UTF-16 and remove null terminator
+            string text = Encoding.Unicode.GetString(szText);
+            string[] substrs = text.Split('$');
+            uint freq0 = uint.Parse(substrs[0]);
+            uint freq1 = uint.Parse(substrs[1]);
+            uint sel = uint.Parse(substrs[2]);
+
+            return sel == 0 ? freq0 : freq1;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                    stream = null;
+                }
+                if (tcpClient != null)
+                {
+                    tcpClient.Close();
+                    tcpClient = null;
+                }
+                if (udpClient != null)
+                {
+                    udpClient.Close();
+                    udpClient = null;
+                }
+
+                txtStatus.AppendText($"[{DateTime.Now:HH:mm:ss}] Disconnected\n\n");
+
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                txtIpAddress.Enabled = true;
+                txtPort.Enabled = true;
+                txtRF2KAddress.Enabled = true;
+                txtRF2KPort.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Disconnect error: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Clean up on form close
+            if (stream != null)
+                stream.Close();
+            if (tcpClient != null)
+                tcpClient.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStatus.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (stream != null && tcpClient.Connected)
+            {
+                // Create and send the message
+                byte[] messageBytes = CreateMessage("get freqx");
+                stream.Write(messageBytes, 0, messageBytes.Length);
+                stream.Flush();
+
+                //txtStatus.AppendText($"[{DateTime.Now:HH:mm:ss}] Sent: {txtMessage.Text}\n");
+
+                // Receive the response
+                byte[] response = ReceiveMessage();
+                if (response != null)
+                {
+                    uint txFrequency = GetTXFrequency(response);
+                    if (prevFreq != txFrequency)
+                    {
+                        prevFreq = txFrequency;
+                        try
+                        {
+                            // Create XML document
+                            var xml = new XElement("RadioInfo",
+                                new XElement("RadioNr", 1),
+                                new XElement("TXFreq", txFrequency / 10),
+                                new XElement("FocusRadioNr", 1)
+                            );
+
+                            XDocument doc = new XDocument(
+                                new XDeclaration("1.0", "utf-8", "yes"), // XML version, encoding and standalone attribute
+                                xml);
+
+                            StringBuilder sb = new StringBuilder();
+                            using (System.IO.TextWriter tr = new System.IO.StringWriter(sb))
+                            {
+                                doc.Save(tr);
+                            }
+
+                            var data = Encoding.UTF8.GetBytes(sb.ToString());
+
+                            // Send UDP packet
+                            udpClient.Send(data, data.Length, udpAddr, udpPort);
+                            //Console.WriteLine($"Sent XML to {_udpHost}:{_udpPort}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error sending UDP packet: {ex.Message}");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            System.GC.Collect();
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            SaveSettings();
+        }
+    }
+}
